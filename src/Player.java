6c5965858081461e351ad837b3df9f7ffd37cd39
edward_mulhern <jamesopti22@gmail.com>
2019-06-06 13:13:50 -0400

    player class

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+public class Player {
+
+
+    private int numOfTroops = 1;
+    private int troopGain = 0;
+    private int remainingRenforcements = 21;
+    private boolean isDefending = true;
+
+
+        public boolean isAlive(int numOfTroops){
+
+            while (numOfTroops <= 0){
+                return false;
+            }
+
+            return true;
+
+        }
+
+}
