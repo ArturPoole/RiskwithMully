913ea4dbd3f0b0077d28f2e6c131d4fdb728c079
artur_poole <artur_poole@student.wayland.k12.ma.us>
2019-06-10 20:49:19 -0400

    Country Class Update

--- src/Country.java
+++ src/Country.java
@@ -1,4 +1,24 @@
+import java.awt.*;
+
 public class Country {
+    private int[] intX;
+    private int[] intY;
+    private int nuSide;
+
+    public Country(int[] xCords, int[] yCords) {
+        intX = xCords;
+        intY = yCords;
+
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.RED);
+        g2.drawPolygon(intX, intY, intX.length);
+
+
+
+
+    }
 
 
 
--- src/Player.java
+++ src/Player.java
@@ -4,16 +4,58 @@ public class Player {
     private int numOfTroops = 1;
     private int troopGain = 0;
     private int remainingRenforcements = 21;
-    private boolean isDefending = true;
+    private boolean isDefending = true, isActive = false;
 
 
-        public boolean isAlive(int numOfTroops){
+    public Player() {}
 
-            while (numOfTroops <= 0){
-                return false;
-            }
 
-            return true;
+
+    public boolean isAlive(int numOfTroops){
+
+        while (numOfTroops <= 0){
+            return false;
         }
+
+        return true;
+    }
+
+
+    public void setActive() {
+        isActive = !isActive;
+    }
+
+    public int getNumOfTroops() {
+        return numOfTroops;
+    }
+
+    public void setNumOfTroops(int numOfTroops) {
+        this.numOfTroops = numOfTroops;
+    }
+
+    public int getTroopGain() {
+        return troopGain;
+    }
+
+    public void setTroopGain(int troopGain) {
+        this.troopGain = troopGain;
+    }
+
+    public int getRemainingRenforcements() {
+        return remainingRenforcements;
+    }
+
+    public int setRemainingRenforcements() {
+        return remainingRenforcements;
+    }
+
+    public boolean isDefending() {
+        return isDefending;
+    }
+
+    public void setDefending(boolean defending) {
+        isDefending = defending;
+    }
+
 
 }
